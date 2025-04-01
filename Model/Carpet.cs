a6b2fc77bf7a9221d4e312eae9716d23f561796b
Billy Hancock <thatdevwedonttalkabout@gmail.com>
2025-04-02 11:30:37 +1300

    Init backend code.

---
+++ Model/Carpet.cs
@@ -0,0 +1,29 @@
+﻿using CarpetCostCalculator.Operations;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CarpetCostCalculator.Model
+{
+    public class Carpet
+    {
+        public List<CarpetType> CarpetTypes { get; set; } = [
+            new CarpetType { Name = "Cheap", PriceSQM = 100, InstallationCostSQM = 20, UnderlayCostSQM = 20 },
+            new CarpetType { Name = "Home", PriceSQM = 200, InstallationCostSQM = 20, UnderlayCostSQM = 20 },
+            new CarpetType { Name = "Luxurious", PriceSQM = 300, InstallationCostSQM = 20, UnderlayCostSQM = 20 }
+        ];
+
+        [BindProperty]
+        public float RoomLength { get; set; }
+
+        [BindProperty]
+        public float RoomWidth { get; set; }
+
+        [BindProperty]
+        public CarpetType SelectedCarpet { get; set; }
+
+        [BindProperty]
+        public bool UseInstallation { get; set; }
+
+        [BindProperty]
+        public bool UseUnderlay { get; set; }
+    }
+}
---
+++ Model/CarpetType.cs
@@ -0,0 +1,13 @@
+﻿namespace CarpetCostCalculator.Model
+{
+    public class CarpetType
+    {
+        public string Name { get; set; }
+
+        public float PriceSQM { get; set; }
+
+        public float InstallationCostSQM { get; set; }
+
+        public float UnderlayCostSQM { get; set; }
+    }
+}
